276033ce6dc9185df3bff5bdc57a90f1ec934dbe
Javier C <youracow@gmail.com>
2013-06-13 16:47:49 -0600

    added notifiers to display where at the program is when downloading entire search

--- examples/geteveryapp.py
+++ examples/geteveryapp.py
@@ -1,6 +1,8 @@
 from gp import Search
 
-fpi = open("search.txt", "a")   # append to file
+FILENAME = "search.txt"
+
+fpi = open(FILENAME, "a")   # append to file
 
 # make a search object supplying the constructor with with term you want to search for
 search = Search("twitter")
@@ -10,7 +12,9 @@ search = Search("twitter")
 for page_num in range(1,20):
     out = ""
     # get apps on page x
+    print "on page", page_num
     page = search.get_page(page=page_num)
+    print "finished reading page", page_num
     #iterate through the apps on page x. there are 24 apps/page
     for app in range(0,24):
         # print page[app].get_name(), page[app].get_permission()
@@ -19,5 +23,6 @@ for page_num in range(1,20):
     out += "\n"
     mystring = out.encode('ascii', 'ignore')
     fpi.write(mystring)
-    # print out
+    print "finished writing page", page_num, "to", FILENAME
+
 
